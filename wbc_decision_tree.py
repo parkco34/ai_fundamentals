1f84b4c4010fa0191d6ea04ceae2496b23d78005
parkco34 <parkercorya@gmail.com>
2024-07-19 17:19:45 -0400

    Redoing a lot because of a lack of time to fully grasp this shit.  I'm working way too much ... (ノಠ益ಠ)ノ彡┻━┻

--- wbc_decision_tree.py
+++ wbc_decision_tree.py
@@ -3,16 +3,42 @@ import numpy as np
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
-# fetch dataset
-breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
 
-# data (as pandas dataframes)
-X = breast_cancer_wisconsin_diagnostic.data.features # float64
-y = breast_cancer_wisconsin_diagnostic.data.targets # float64
-# Encode target variables as float
-y_class = y["Diagnosis"].map({"M": 1, "B": 0}).astype(float)
+def read_data(filename, test=False):
+    """
+    Reads data from csv file.
+    ----------------------------------------
+    INPUT:
+        filename: (str) Filename including path.
+        test: (bool; default: False) Determines how to obtain data, either for
+        testing or not.
 
-# 1)
+    OUTPUT:
+        (tuple)
+    ----------------------------------------
+    TEST CASE
+        best_attribute = "concavity1"
+        best_ig = 0.36082517699473016
+    """
+    if not test:
+        # fetch dataset
+        breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
+
+        # data (as pandas dataframes)
+        X = breast_cancer_wisconsin_diagnostic.data.features # float64
+        y = breast_cancer_wisconsin_diagnostic.data.targets # float64
+        # Encode target variables as float
+        y = y["Diagnosis"].map({"M": 1, "B": 0}).astype(float)
+        return X, y
+
+    # Test case
+    else:
+        X = pd.read_csv(filename)
+        y = X["Diagnosis"].map({"M": 1, "B": 0}).astype(float)
+        X.drop("Diagnosis", axis=1,  inplace=True)
+
+        return X, y
+
 def entropy(y):
     """
 
@@ -29,57 +55,77 @@ def entropy(y):
     # Binary Entropy function
     return -sum(probability * np.log2(probability))
 
-# 2)
-
-def weighted_child_entropy(X, y, attribute):
+def split_data(X, y, attribute, value):
     """
-    Because child nodes often have different # of of samples, this ensures we account for the relative importance of each child node based on
-    its size.
-    --------------------------------
+    Splits the data into two groups based on the given attribute and unique
+    value in attribute (column).
+    ---------------------------------------
     INPUT:
         X: (pd.DataFrame) Attribute data
         y: (pd.Series) Target data
-        attribute: (str)
+        attribute: (str) The attribute to split the data on
+        value: (float) The value to use as the threshold for the split
 
     OUTPUT:
-        (float)
+        left: (tuple) The left group of data and target values
+        right: (tuple) The right group of data and target values
     """
-    # Unique values for some reason ????
-    unique_values = X[attribute].unique()
-    
-    weighted_entropy = 0
-    total_samples = len(y)
-    counter=0
+    # Create masks to split the data into left and right groups
+    left_mask = X[attribute] < value
+    right_mask = X[attribute] >= value
+    left = X[left_mask], y[left_mask]
+    right = X[right_mask], y[right_mask]
 
-    for value in unique_values:
-        # Mask to use in obtaining subset of corrsponding target values
-        mask = X[attribute] == value
-        y_subset = y[mask]
-        # The # of elements in the subset divided by the # of total samples
-        weight = len(y_subset) / total_samples
-        # Get subset entropy in order to multiply it by the weights for the
-        # WEIGHTED AVERAGE
-        subset_entropy = entropy(y_subset)
-        # Finally
-        weighted_entropy += weight * subset_entropy
+    return left, right
+
+def weighted_child_entropy(left_y, right_y):
+    """
+    How do we calculate entropy after performing a split ?
+    Because child nodes often have different # of of samples, this ensures we account for the relative importance of each child node based on
+    its size.
+    -----------------------------------------------------------
+    H(S|A) = Σ (|S_v| / |S|) * H(S_v)
+
+    In this formula, |S_v| represents the number of instances in subset 
+    v, and |S| represents the total number of instances in S.
+    ------------------------------------------------------------
+    INPUT:
+        left_y: (pd.Series) Target data of left child node
+        right_y: (pd.Series) Attribute data of right child node
+
+    OUTPUT:
+        weighted_entropy: (float) weighted entropy of the kiddos
+    """
+    total_samples = len(left_y) + len(right_y)
+    left_weight = len(left_y) / total_samples
+    right_weight = len(right_y) / total_samples
+    weighted_entropy = left_weight * entropy(left_y) + (right_weight *
+    entropy(right_y))
 
     return weighted_entropy
 
-def information_gain(X, y, attribute):
+def information_gain(X, y, attribute, value):
     """
-    CHOOSING THE MOST IMPORTANT ATTRIBUTE TO TEST FIRST.
-    We want higher information gain ~ highest reduction in entropy,
-    substracting weighed entropy of child nodes from entropy of parent node.
     ----------------------------------------------------
     INPUT:
         X: (pd.DataFrame) Attribute data
         y: (pd.Series) Target data
         attribute: (str)
+        value: (float) Value used as threshold for split
 
     OUTPUT:
-        (float)
+        (float) Information gain
     """
-    return entropy(y) - weighted_child_entropy(X, y, attribute)
+    # Splitting the data
+    left, right = split_data(X, y, attribute, value.item())
+    left_y, right_y = left[1], right[1]
+
+    # Parent entropy
+    parent = entropy(y)
+    # Weighted entropy pf kids
+    child_entropy = weighted_child_entropy(left_y, right_y)
+    
+    return parent - child_entropy
 
 def find_best_attribute(X, y):
     """
@@ -90,20 +136,25 @@ def find_best_attribute(X, y):
         y: (pd.Series) Target data
         
     OUTPUT:
-        best_attribute: ?
+        best_attribute: (str) Column name
     """
     best_attribute = None
     best_ig = -1 # Since it has to be positive
-
+  
+    # Loop over unique values to determine best attribute via info gain
     for attribute in X.columns:
-        ig = information_gain(X, y, attribute)
+        unique_values = X[attribute].unique()
 
-        if ig > best_ig:
-            best_ig = ig
-            best_attribute = attribute
+        # Iterate thru unique values to find best value
+        for value in unique_values:
+            info_gain = information_gain(X, y, attribute, value)
+            # Test for best attribute
+            if info_gain > best_ig:
+                best_ig = info_gain
+                best_attribute = attribute
 
     return best_attribute
-
+    
 def grow_tree(X, y, max_depth=None, min_samples_split=2, current_depth=0):
     """
     Divide and conquer.
@@ -114,26 +165,25 @@ def grow_tree(X, y, max_depth=None, min_samples_split=2, current_depth=0):
         max_depth: (int, default: None) Maxmium depth of tree
         min_samples_split: (int, default: 2) Min # of attributes to split
 
-    OUTPUT: """
+    OUTPUT: 
+        tree: (nested dict) Decision tree
+    """
     # == BASE CASES ==
     # 1) All samples in current node belong to same class (pure node)
     if len(y.unique()) == 1:
-        return y.unique()[0]
+        return y.iloc[0]
 
     # 2) max_depth reached: return MOST COMMON CLASS
     if max_depth is not None and current_depth >= max_depth:
-        return max(np.bincount(y))
+        return y.mode().iloc[0]
 
     # 3) # of samples < minimum # of samples required to split, returning the MOST
     # COMMON CLASS
     if len(y) < min_samples_split:
+        """ Change this shit ? """
         return max(np.bincount(y))
 
-    # 4) No attributes to split on
-    if len(X.columns) == 0:
-        return max(np.bincount(y))
-
-    # find best attribute to split
+    # ROOT NODE 
     best_attribute = find_best_attribute(X, y)
 
     # Creating tree structure ??
@@ -141,24 +191,93 @@ def grow_tree(X, y, max_depth=None, min_samples_split=2, current_depth=0):
     
     # Splitting on best attribute
     for value in X[best_attribute].unique():
+        
+        # Value as float, not np.float64
+        value = value.item()
         # Create mask for current value, where the item() part converts
-        # np.float64 to float
-        mask = X[best_attribute] == value.item()
+        mask = X[best_attribute] == value
         # Create new dataframe subset, removing best_attribute
         X_subset = X.drop(columns=[best_attribute])
+
+        # Remove index of value in y
+        idx = y[mask].index[0].item() # ?
+        y_subset = y.drop(idx, axis=0) # ?
+        
         # Recursively grow tree for subset
         subtree = grow_tree(
             X_subset,
-            y[mask],
+            y_subset[mask],
             max_depth,
             min_samples_split,
             current_depth + 1
         )
-        
+
         tree[best_attribute][value] = subtree
 
+    return tree
+
+def predict(tree, sample):
+    """
+    Makes prediction for single sample.
+
+    Start at root node. check attribute value in sample.  Based on value, move
+    to corresponding branch of tree, otherwise return leaf node.
+    ---------------------------------------------
+    INPUT:
+        tree: (dict) Decision tree
+        sample: (pd.DataFrame) Attribute dataframe
+
+    OUTPUT:
+        prediction: (float) Predicted class
+    """
+    # Reached leaf node to be returned
+    if isinstance(tree, float):
+        return tree
+
+    # Get attribute for current node
+    attribute = list(tree.keys())[0]
+
+    # Get value of this attribute in sample
+    value = sample[attribute]
+
+    # COnvert to python float if np.float64
+    if isinstance(value, np.float64):
+        value = value.item()
+
+    # If value exists in our tree, treverse to correpsonding subtree
+    if value in tree[attribute]:
+        return predict(tree[attribute][value], sample)
+    
+    else:
+        # if value not in tree, reutrn most common class
+        return max(tree[attribute], key=lambda x: sum(isinstance(v, float) for
+                                                     v in
+                                                      tree[attribute][x].values()))
+
+def predict_all(tree, X):
+    """
+    Make predictions for multiple samples using the decision tree.
+    ---------------------------------------------------
+    INPUT:
+        tree: (nested dict) Decision tree
+        X: (pd.DataFrame) Samples to predict
+
+    OUTPUT:
+        predictions: ()
+    """
+    pass
 
 
-grow_tree(X, y_class, max_depth=5, min_samples_split=5, current_depth=0)
+
+data = read_data("data/partial_dataset.csv", True)
+X, y = data[0], data[1]
 
 breakpoint()
+
+
+#def main():
+#    data = read_data("data/partial_dataset.csv", True)
+#    X, y = data[0], data[1]
+#
+#
+#if __name__ == "__main__":
