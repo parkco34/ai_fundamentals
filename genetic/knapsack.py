e3e4562a34cb791c84de48cfe6d01194ae824db8
parkco34 <parkercorya@gmail.com>
2024-08-25 18:17:27 -0400

    Finally finished.  I still gotta refactor and test, then do the damn report ...

--- genetic/knapsack.py
+++ genetic/knapsack.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-import logging
-from random import uniform, sample, randint, random
-
-# Setting up logging
-logging.basicConfig(filename="example_population.log", level=logging.INFO,
-                    format="%(message)s")
-
-knapsack_data = {
+"""
+0/1 Knapsack Problem -Completed: 8/25/24
+-----------------------------------------------------------------------------
+-------------
+Example usage:
+-------------
+data = {
     "capacity": 20,
     "items": [
         {"weight": 1, "value": 3},
@@ -18,10 +17,23 @@ knapsack_data = {
     ]
 }
 
-# Population size determined via "small" for simple problem
 POP_SIZE = 50
 ITEMS = [(item["weight"], item["value"]) for item in knapsack_data["items"]]
 CAPACITY = knapsack_data["capacity"]
+population = get_population(data["items"], POP_SIZE)
+-----------------------------------------------------------------------------
+"""
+import logging
+import numpy as np
+import random
+from random import uniform, sample, randint
+
+# Setting up logging
+logging.basicConfig(filename="example_population.log", level=logging.INFO,
+                    format="%(message)s")
+
+def generate_seed():
+    return randint(0, 2 ** 32 - 1)
 
 def get_data(config_file):
     """
@@ -36,7 +48,15 @@ def get_data(config_file):
         items: (list of tuples) weight-value pairs
         stop: (int) Stopping condition
     """
-    pass
+    with open(config_file, "r") as file:
+        lines = file.readlines()
+        pop_size = int(lines[0].strip())
+        n = int(lines[1].strip())
+        stop = int(lines[2].strip())
+        W = int(lines[3].strip())
+        S = [tuple(map(int, line.strip().split())) for line in lines[4:]]
+
+    return pop_size, n, stop, W, S
 
 def get_population(data, pop_size):
     """
@@ -51,6 +71,11 @@ def get_population(data, pop_size):
     OUTPUT:
         population: (list)
     """
+    # INput validation
+    if not (isinstance(data, list) and isinstance(data[0], dict)):
+        raise ValueError("""{20 * "*"}\n\nInvalid input!\n\nInput needs to be a list of
+                         dictionaries\n\n{20 * "*"}""")
+
     # Getting unique population 
     population = []
     while len(population) < pop_size:
@@ -59,7 +84,6 @@ def get_population(data, pop_size):
         # Ensure uniqueness
         if chromosome not in population:
             population.append(chromosome)
-
 
     # Log population
     logging.info("Generated Population")
@@ -164,30 +188,28 @@ def tournament_selection(population, p=0.75, tournament_size=3):
         parent1, parent2: (list of lists)
     """
     parents = []
-    cumulative_prob = 0
-    remaining_prob = 1
-    count = 0
-   
-    # If tournament size None, include 20% of the population
-    if not tournament_size:
-        tournament_size = int(.20 * len(population))
+    
+    # Tournament size not greater than population size
+    tournament_size = min(tournament_size, len(population))
 
-    contestants = sample(population, tournament_size)
-    # Sort contestants based on their fitness
-    contestants.sort(key=fitness, reverse=True)
-   
-    while len(parents) < 2 and count < 50: # ?
-        count += 1
+    while len(parents) < 2:
+        contestants = sample(population, tournament_size)
+        contestants.sort(key=fitness, reverse=True)
 
-        for i in range(len(contestants)):
-            cumulative_prob += p * (remaining_prob)
-            remaining_prob *= (1 - p)
+        # Select best individual with probability p
+        if random() < p:
+            winner = contestants[0]
 
-            if random() <= cumulative_prob and contestants[i] not in parents:
-                parents.append(contestants[i])
+        else:
+            # Not best
+            winner = contestants[randint(1, tournament_size - 1)]
+        
+        # Uniqueness of parents
+        if winner not in parents:
+            parents.append(winner)
 
-                if len(parents) == 2:
-                    break
+    if len(parents) < 2:
+        parents.append(parents[0])
 
     return parents[0], parents[1]
 
@@ -207,7 +229,8 @@ def valid_solution(chromosome, items, capacity): # ?
     return total_weight <= capacity
 
 # Crossover
-def single_point_crossover(parent1, parent2, crossover_rate=0.7):
+def single_point_crossover(parent1, parent2, crossover_rate=0.7,
+                           mutation_rate=0.01):
     """
     Single-point crossover between two abusive parents, splitting at a random
     point in the chromosome, where the the genes after that point will be
@@ -217,6 +240,7 @@ def single_point_crossover(parent1, parent2, crossover_rate=0.7):
         parent1: (list) 
         parent2: (list)
         crossover_rate: (float; default=0.7) Probability of 
+        mutation_rate: (float; default=0.01) Probability of mutating a bit
 
     OUTPUT:
         child1, child2: (list of lists) Offspring
@@ -236,7 +260,9 @@ def single_point_crossover(parent1, parent2, crossover_rate=0.7):
     else:
         child1, child2 = parent1[:], parent2[:]
 
-    # Sanity check ?
+    # Mutants
+    child1 = bit_flip_mutation(child1)
+    child2 = bit_flip_mutation(child2)
 
     return child1, child2
 
@@ -244,32 +270,93 @@ def k_point_crossover():
     pass
 
 # Mutation
-def bit_flip_mutation(child):
+def bit_flip_mutation(child, mutation_rate=0.01):
     """
     Mutation of child via BIT-FLIP, determined by the mutationrate:
     ~ 1 / len(child).
     ---------------------------------------------
     INPUT:
         child: (list)
+        mutation_rate: (float; default=0.01) Probability of mutating a bit
 
     OUTPUT:
         mutated_child: (list)
     """
-    mutation_rate = 1 / len(child)
-    
-    mutated_child = [1 - gene if random() < mutation_rate else gene for
-                          gene in child]
+    if isinstance(child, list):
+        # Copy child
+        mutated_child = child[:]
+        
+        for i in range(len(mutated_child)):
+            if random() <= mutation_rate:
+                # FLips bit
+                mutated_child[i] = 1 - mutated_child[i]
 
     return mutated_child
 
-def main():
-    pass
+# ?
+def create_new_population(population, elite_size=2):
+    """
+    Generates a new population via elitism ...
+    ---------------------------------------------
+    INPUT:
+        population: (list)
+        elite_size: (int; default=2) Number of the best fit individuals to
+        carry on to the next generation, w/out any crossover or mutation
 
-population = get_population(knapsack_data["items"], 50)
-lst = []
-for i in range(5000):
-    parent1, parent2 = tournament_selection(population)
-    child1, child2 = single_point_crossover(parent1, parent2)
-    mutant1, mutant2 = bit_flip_mutation(child1), bit_flip_mutation(child2)
+    OUTPUT:
+        new_population: (list of lists) For next generation
+    """
+    # Sort based on fitness
+    sorted_population = sorted(population, key=fitness, reverse=True)
+    
+    # Elists
+    new_population = sorted_population[:elite_size]
+    
+    # Iteratively generate new population of individuals
+    while len(new_population) < len(population):
+        # ? --> Replace specific function with parameter
+        parent1, parent2 = tournament_selection(sorted_population)
+        child1, child2 = single_point_crossover(parent1, parent2)
 
-breakpoint()
+        if len(new_population) < len(population):
+            new_population.append(child1)
+            new_population.append(child2)
+
+    # ? --> Input validation
+    
+    return new_population 
+
+def main(config_file, seed=None):
+    if seed is None:
+        seed = generate_seed()
+
+    # Set random seed
+    random.seed(seed)
+    np.random.seed(seed)
+        
+    print(f"Using random seed: {seed}")
+
+    # COnstants
+    global POP_SIZE, ITEMS, CAPACITY
+    POP_SIZE, N, STOP, CAPACITY, ITEMS = get_data(config_file)
+
+    population = get_population(ITEMS, POP_SIZE)
+
+    generation = 0
+    max_generations = 100
+
+    breakpoint()
+    while generation < max_generations:
+        population = create_new_population(population)
+        best_solution = max(population, key=fitness)
+        logging.info(f"""Generation {generation}: Best solution {best_solution}
+                    with fitness {fitness(best_solution)}""")
+        generation += 1
+
+    best_solution = max(population, key=fitness)
+    print(f"Best solution is {fitness(best_solution)}")
+    print(f"Winner: {best_solution}")
+
+
+if __name__ == "__main__":
+    main("data/config_1.txt")
