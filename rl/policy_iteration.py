7f156a29927f61a6c05c71dc20a200e581af2340
parkco34 <parkercorya@gmail.com>
2024-10-04 11:07:58 -0400

    Starting the CartPole problem for RL.

--- rl/policy_iteration.py
+++ rl/policy_iteration.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
 """
-POLICY ITERATION
+POLICY ITERATION Assignement:
 Implement policy evaluation and improvement to find optimal policy.
+----------------------------------------------------------------------
+Learning Objectives
+• Implement and understand policy iteration, temporal difference (TD) learning, and Monte Carlo (MC)
+methods.
+• Apply discretization techniques to handle continuous state spaces in tabular methods.
+• Utilize function approximation to manage high-dimensional or continuous state spaces.
+• Compare the performance of different RL algorithms and understand their advantages and limitations.
 """
 import gym
 import numpy as np
 
 env = gym.make("CartPole-v0")
-actions = [0, 1]
-alpha = 0.1
-gamma = 1.0
-epsilon = 0.1
-num_iterations = 10
-num_episodes = 1000
+# My attempt ?
+state_space = 4
+action_space = 2
 
-# initialize policy and value function
-policy = {}
-value_function = {}
+def Qtable(state_space, action_space, bin_size=30):
+    bins = [
+        np.linspace(-4.8, 4.8, bin_size),
+        np.linspace(-4, 4, bin_size),
+        np.linspace(-0.418, 0.418, bin_size),
+        np.linspace(-4, 4, bin_size)
+    ]
 
-for state in all_possible_states:
-    policy[state] = np.random.choice(actions)
-    value_function[state] = 0.0
+    q_table = np.random.uniform(low=-1, high=1, size=([bin_size] * state_space
+                                                     + [action_space]))
 
-for i in range(num_iterations):
-    # Policy evaluation
-    for episode in range(num_episodes):
-        observation = env.reset()
-        state = discretize_state(observation)
-        done = False
+    return q_table, bins
 
-        while not done:
-            action = policy[state]
+def discrete(state, bins):
+    index = []
+
+    for i in range(len(state)):
+        index.append(np.digitize(state[i], bins[i]) - 1)
+
+    return tuple(index)
 
 
