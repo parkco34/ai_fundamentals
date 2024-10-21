0368c3387a3fe41e6e9e0635ebd919b592269585
parkco34 <parkercorya@gmail.com>
2024-10-20 21:15:38 -0400

    Started Part 2, using Neural Networks.

--- rl/cartpole_part1.py
+++ rl/cartpole_part1.py
@@ -83,6 +83,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import itertools
+# PART 2
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 # Example values
 EXAMPLE_EPSILON = 1.0
@@ -96,6 +100,8 @@ EXAMPLE_EPISODES = 5000
 EXAMPLE_EPSILON_MIN = 0.01
 EXAMPLE_NUM_SAMPLES = 5000
 EXAMPLE_MAX_STEPS = 500
+# PART 2
+EXAMPLE_HIDDEN_UNITS = 64
 
 # Set random seeds for reproducibility
 np.random.seed(73)
@@ -649,57 +655,32 @@ def performance_evaluation(rewards_list, algorithm_name):
     plt.ylabel("Total Reward")
     plt.show()
 
-def main():
-    # Initialize Q-table and bins
-    q_table, bins = Qtable(bin_size=EXAMPLE_BIN_SIZE)
-
-    # Q-Learning
-    q_learning_rewards, q_learning_q_table = q_learning(
-        q_table = np.copy(q_table),
-        bins = bins,
-        episodes = EXAMPLE_EPISODES,
-        discount = EXAMPLE_DISCOUNT,
-        alpha = EXAMPLE_ALPHA,
-        timestep = EXAMPLE_TIMESTEP,
-        epsilon = EXAMPLE_EPSILON,
-        epsilon_decay = EXAMPLE_EPSILON_DECAY,
-        epsilon_min = EXAMPLE_EPSILON_MIN
-    )
-    performance_evaluation(q_learning_rewards, "Q-Learning")
-
-    # SARSA
-    sarsa_rewards, sarsa_q_table = sarsa(
-        q_table = np.copy(q_table),
-        bins = bins,
-        episodes = EXAMPLE_EPISODES,
-        discount = EXAMPLE_DISCOUNT,
-        alpha = EXAMPLE_ALPHA,
-        timestep = EXAMPLE_TIMESTEP,
-        epsilon = EXAMPLE_EPSILON,
-        epsilon_decay = EXAMPLE_EPSILON_DECAY,
-        epsilon_min = EXAMPLE_EPSILON_MIN
-    )
-    performance_evaluation(sarsa_rewards, "SARSA")
-
-    # Monte Carlo
-    mc_rewards, mc_q_table = monte_carlo(
-        q_table = np.copy(q_table),
-        bins = bins,
-        episodes = EXAMPLE_EPISODES,
-        discount = EXAMPLE_DISCOUNT,
-        epsilon = EXAMPLE_EPSILON,
-        epsilon_decay = EXAMPLE_EPSILON_DECAY,
-        epsilon_min = EXAMPLE_EPSILON_MIN
-    )
-    performance_evaluation(mc_rewards, "Monte Carlo")
-
 """
-PART 2
+PART 2 ========================================================
 """
 
-# Initialize Parameters
-num_features = env.observation_space.shape[0] + 1
-theta = np.zeros(num_features)
+class QNetwork(nn.module):
+    """
+    QNetwork class.
+    """
+
+    def __init__(self, state_size, action_size,
+                 hidden_units=EXAMPLE_HIDDEN_UNITS):
+        super(QNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(state_size, hidden_units)
+        self.fc2 = nn.Linear(hidden_units, hidden_units)
+        self.fc3 = nn.Linear(hiddent_units, action_size)
+
+    def forward(self, state):
+        """
+        ?
+        """
+        x = torch.reul(self.fc1(state))
+        x = torch.relu(self.fc2)
+        x = self.fc3(x)
+
+        return x
 
 # Linear function approx
 def get_features(state, action):
@@ -788,6 +769,70 @@ def linear_q_learning(
     env.close()
 
     return epsiode_rewards, theta
+
+def main():
+    # Initialize Q-table and bins
+    q_table, bins = Qtable(bin_size=EXAMPLE_BIN_SIZE)
+
+    # Q-Learning
+    q_learning_rewards, q_learning_q_table = q_learning(
+        q_table = np.copy(q_table),
+        bins = bins,
+        episodes = EXAMPLE_EPISODES,
+        discount = EXAMPLE_DISCOUNT,
+        alpha = EXAMPLE_ALPHA,
+        timestep = EXAMPLE_TIMESTEP,
+        epsilon = EXAMPLE_EPSILON,
+        epsilon_decay = EXAMPLE_EPSILON_DECAY,
+        epsilon_min = EXAMPLE_EPSILON_MIN
+    )
+    performance_evaluation(q_learning_rewards, "Q-Learning")
+
+    # SARSA
+    sarsa_rewards, sarsa_q_table = sarsa(
+        q_table = np.copy(q_table),
+        bins = bins,
+        episodes = EXAMPLE_EPISODES,
+        discount = EXAMPLE_DISCOUNT,
+        alpha = EXAMPLE_ALPHA,
+        timestep = EXAMPLE_TIMESTEP,
+        epsilon = EXAMPLE_EPSILON,
+        epsilon_decay = EXAMPLE_EPSILON_DECAY,
+        epsilon_min = EXAMPLE_EPSILON_MIN
+    )
+    performance_evaluation(sarsa_rewards, "SARSA")
+
+    # Monte Carlo
+    mc_rewards, mc_q_table = monte_carlo(
+        q_table = np.copy(q_table),
+        bins = bins,
+        episodes = EXAMPLE_EPISODES,
+        discount = EXAMPLE_DISCOUNT,
+        epsilon = EXAMPLE_EPSILON,
+        epsilon_decay = EXAMPLE_EPSILON_DECAY,
+        epsilon_min = EXAMPLE_EPSILON_MIN
+    )
+    performance_evaluation(mc_rewards, "Monte Carlo")
+
+    """
+    PART 2 ========================================================
+    """
+    # Initialize theta
+    linear_theta = np.zeros(num_feaures)
+
+    # Run linear Q-Learning
+    q_learning_rewards, linear_theta = q_learning(
+        theta = linear_theata,
+        episodes = EXAMPLE_EPISODES,
+        discount = EXAMPLE_DISCOUNT,
+        alpha = EXAMPLE_ALPHA,
+        episilon = EXAMPLE_EIPSLION,
+        epsilon_decay = EXAMPLE_EPSILON_DECAY,
+        epsilon_min = EXAMPLE_EPSILON_MIN
+    )
+
+    # Evaluate performance
+    performance_evluation(q_learning_rewards, "Linear Q-Learning")
 
 
 if __name__ == "__main__":
